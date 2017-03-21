75afcbf2b19d76cf7f199537b51ddb3695891064
steven wang <steven wang>
2017-03-21 20:45:38 +1300

    refactor hero class and hero detail
    Signed-off-by: steven wang <steven wang>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
+﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
